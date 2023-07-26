85db6ab412be1a6c972dacf2807de5b2069b52f8
vedu111 <139427232+vedu111@users.noreply.github.com>
2023-07-26 21:28:51 +0530

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-document.querySelector("#increment").addEventListener("click", function () {
-   var number = Number(document.getElementById('number').innerHTML);
-
-   document.getElementById('number').innerHTML = number + 1;
-
+document.querySelector(".increment-btn").addEventListener("click", function () {
+  var number = Number(document.getElementById('number').innerHTML);
+  document.getElementById('number').innerHTML = number + 1;
 });
 
-
-document.querySelector("#decrement").addEventListener("click", function () {
-   var number = Number(document.getElementById('number').innerHTML);
-
-   document.getElementById('number').innerHTML = number - 1;
-
+document.querySelector(".decrement-btn").addEventListener("click", function () {
+  var number = Number(document.getElementById('number').innerHTML);
+  document.getElementById('number').innerHTML = number - 1;
 });
 
-
-document.querySelector("#neutral").addEventListener("click", function () {
-   document.getElementById('number').innerHTML = 0;
+document.querySelector(".neutral-btn").addEventListener("click", function () {
+  document.getElementById('number').innerHTML = 0;
 });
 
+document.querySelector(".save-btn").addEventListener("click", function () {
+  var number = Number(document.getElementById('number').innerHTML);
+  var saveDiv = document.getElementById("save");
+  var currentEntries = saveDiv.textContent;
+  var countStr = number + ", ";
+  saveDiv.textContent = currentEntries + countStr;
+  document.getElementById('number').innerHTML = 0;
+});
